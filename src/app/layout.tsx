dccc992801919b59f3d871b681c8c8d382e89cba
arunshekhar <arunshekhar001@gmail.com>
2025-08-10 22:53:57 +0530

    anal added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { ChakraProviderWrapper } from "./_features/shared/components/ui/provider"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ChakraProviderWrapper>{children}</ChakraProviderWrapper>
+        {process.env.NODE_ENV === "production" && <Analytics />}
       </body>
     </html>
   )
